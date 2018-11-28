3d95e6bb6ca3d1edb2cba3032dd3bbdb374b2043
SergioEstebanP <sergi2312@gmail.com>
2018-11-28 03:14:09 +0100

    remove some no neede labels

--- src/test/java/com/hfc/demo/serenitySteps/UserSteps.java
+++ src/test/java/com/hfc/demo/serenitySteps/UserSteps.java
@@ -15,30 +15,30 @@ public class UserSteps {
     private RyanairHome ryanairHome;
     private RyanairResults ryanairResults;
 
-    @Step("Given I am in ryanair webpage")
+    @Step
     public void i_am_in_Ryanair_Webpage() {
         ryanairHome.open();
         ryanairHome.waitLoad();
     }
 
-    @Step("And I close cookies politics")
+    @Step
     public void i_close_cookies_politics() {
         ryanairHome.closeCookiesPopUp();
     }
 
-    @Step("When I click for '<direction>'")
+    @Step
     public void i_click_for(String direction) {
         Serenity.setSessionVariable("direction").to(direction);
         ryanairHome.selectDirection(direction);
     }
 
-    @Step("And I want to go from '<fromPlace>' to '<toPlace>'")
+    @Step
     public void i_want_to_go_from_to(String from, String to) {
         ryanairHome.writeOrigin(from);
         ryanairHome.writeDestination(to);
     }
 
-    @Step("And I want to go tomorrow")
+    @Step
     public void i_want_to_go_tomorrow() {
         Calendar tomorrow = Calendar.getInstance();
         tomorrow.setTime(new Date());
@@ -46,17 +46,17 @@ public class UserSteps {
         ryanairHome.setStartingDate(tomorrow);
     }
 
-    @Step("And I click in go button")
+    @Step
     public void i_click_in_go_button() {
         ryanairResults = ryanairHome.clickGoButton();
     }
 
-    @Step("Then I should see the result page with at least one flight")
+    @Step
     public void i_should_see_at_least_one_flight() {
         Assert.assertTrue("At least one fligth should be found", ryanairResults.getNumberOfResults() >= 1);
     }
 
-    @Step("And with dates: '<dateOut>'")
+    @Step
     public void with_start_date(String entranceDay) {
         try {
             Date entranceDate = DateFormat.getDateInstance(DateFormat.LONG, Locale.ENGLISH).parse(entranceDay);
@@ -72,7 +72,7 @@ public class UserSteps {
         }
     }
 
-    @Step("And '<dateReturn>'")
+    @Step
     public void with_return_date(String dayReturn) {
         if (Serenity.sessionVariableCalled("direction").equals("idaVuelta")) {
             try {
